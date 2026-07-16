faccf3e75b92cb0f5bfe4a161d16987dc814e65b
denispak-bit <pakdenisdmtr@gmail.com>
2026-07-16 08:19:49 +0500

    chore: remove scratch files that broke the build

--- scratch.ts
+++
@@ -1,1 +0,0 @@
-﻿import { ItigrisRemoteClient } from './src/lib/itigris/remote'; const client = new ItigrisRemoteClient({ client: 'optika_narodnaya', key: 'ae8207fa-00eb-43db-b985-06fada966196' }); client.remainsList('glasses', undefined, 1).then(console.log).catch(console.error);
--- scratch_db.ts
+++
@@ -1,1 +0,0 @@
-﻿import { PrismaClient } from '@prisma/client'; const prisma = new PrismaClient(); async function main() { const logs = await prisma.itigrisSyncLog.findMany({ orderBy: { syncedAt: 'desc' }, take: 1, select: { details: true, durationMs: true } }); console.log(JSON.stringify(logs, null, 2)); } main().finally(() => prisma.());
