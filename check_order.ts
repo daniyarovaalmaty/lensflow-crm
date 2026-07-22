9e6f3818f795eec47399ba59022515e4b0c1430f
denispak-bit <pakdenisdmtr@gmail.com>
2026-07-22 17:08:27 +0500

    chore: remove debug scripts breaking build

--- check_order.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from './src/lib/db/prisma';
-
-async function main() {
-  const order = await prisma.order.findFirst({ where: { externalId: 'itigris:1000502164' } });
-  console.log('ORDER DATA:', JSON.stringify(order, null, 2));
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
