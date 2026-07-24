acced338b4a4c414bdd485c9ebc17d639644172b
Ruslanovna <daniyarovaruslanovna@gmail.com>
2026-07-24 17:40:23 +0500

    Fix build by removing scratch files

--- scripts/check_products.ts
+++
@@ -1,6 +0,0 @@
-import prisma from '../src/lib/db/prisma';
-async function main() {
-    const products = await prisma.product.findMany({ where: { category: 'lens' } });
-    console.log(JSON.stringify(products, null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
